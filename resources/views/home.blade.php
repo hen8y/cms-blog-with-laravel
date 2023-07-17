571cdb80dd096854bd685be4c9e23fb8b17423af
Ogbonna Henry <dheryhenry@gmail.com>
2023-07-17 16:32:32 +0100

    Second commit with blog post in blade fmt

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,49 @@
 <x-home-master>
-@extends('layouts.app')
-
 @section('content')
+
+<!-- Single comment-->
+<div class="card-body">
+    <div class="d-flex">
+        <div class="flex-shrink-0"><img class="rounded-circle" src="https://dummyimage.com/50x50/ced4da/6c757d.jpg" alt="..." /></div>
+        <div class="ms-3">
+            <div class="fw-bold">Commenter Name</div>
+            When I look at the universe and all the ways the universe wants to kill us, I find it hard to reconcile that with statements of beneficence.
+        </div>
+    </div>
+</div>
+
+<!-- Single comment-->
+<div class="card-body">
+    <div class="d-flex">
+        <div class="flex-shrink-0"><img class="rounded-circle" src="https://dummyimage.com/50x50/ced4da/6c757d.jpg" alt="..." /></div>
+        <div class="ms-3">
+            <div class="fw-bold">Commenter Name</div>
+            When I look at the universe and all the ways the universe wants to kill us, I find it hard to reconcile that with statements of beneficence.
+        </div>
+    </div>
+</div>
+
+<!-- Single comment-->
+<div class="card-body">
+    <div class="d-flex">
+        <div class="flex-shrink-0"><img class="rounded-circle" src="https://dummyimage.com/50x50/ced4da/6c757d.jpg" alt="..." /></div>
+        <div class="ms-3">
+            <div class="fw-bold">Commenter Name</div>
+            When I look at the universe and all the ways the universe wants to kill us, I find it hard to reconcile that with statements of beneficence.
+        </div>
+    </div>
+</div>
+
+<!-- Single comment-->
+<div class="card-body">
+    <div class="d-flex">
+        <div class="flex-shrink-0"><img class="rounded-circle" src="https://dummyimage.com/50x50/ced4da/6c757d.jpg" alt="..." /></div>
+        <div class="ms-3">
+            <div class="fw-bold">Commenter Name</div>
+            When I look at the universe and all the ways the universe wants to kill us, I find it hard to reconcile that with statements of beneficence.
+        </div>
+    </div>
+</div>
+
 @endsection
+</x-home-master>
