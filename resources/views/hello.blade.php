8022d7a92c5631e8d6ae1951b0be2f84d71812af
Ogbonna Henry <dheryhenry@gmail.com>
2023-08-15 23:39:11 +0100

    ff

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,7 @@
+<head>
+ <link href="{{ asset('css/styles.css')}}" rel="stylesheet">
+</head>
+<body>
+ <h1 class="text-2xl text-red-400 text-center font-bold underline">Welcome to Linuxhint!</h1>
+</body>
+</html>
